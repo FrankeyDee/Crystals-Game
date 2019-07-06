8d0ac0d5ed569bc53acfb6ee6a282efa99ef610a
frankey darban <frankeydarban@gmail.com>
2019-07-06 12:21:52 -0700

    updated JS file for gem functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,97 @@
+		
+	$(document).ready(function(){
+
+
+        //variables
+    
+    
+        let counter = 0;
+        let wins= 0;
+        let losses = 0;
+    
+        
+    
+        function randomNum(min,max){
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max-min +1)) + min;
+    
+        }
+    
+        let winNumber = randomNum(40,90);
+        $("#target").html(winNumber);
+    
+        crystalOne= randomNum(1, 12);
+        crystalTwo= randomNum(1, 12);
+        crystalThree= randomNum(1, 12);
+        crystalFour= randomNum(1, 12);
+    
+    
+        function reset(){
+            counter = 0;
+            $("#target").html(winNumber);
+            winNumber = randomNum(40,90);
+            $("#playerScore").text(counter);
+            crystalOne= randomNum(1, 12);
+            crystalTwo= randomNum(1, 12);
+            crystalThree= randomNum(1, 12);
+            crystalFour= randomNum(1, 12);
+
+        }
+    
+        
+        $("#gem1").on("click", function(){
+            counter += crystalOne;
+            WinLoose();
+            $(playerScore).text(counter);
+            
+        });
+    
+        $("#gem2").on("click", function(){
+            counter += crystalTwo;
+            WinLoose();
+            $(playerScore).text(counter);
+            
+    
+        });
+    
+        $("#gem3").on("click", function(){
+            counter += crystalThree;
+            WinLoose();
+            $(playerScore).text(counter);
+            
+    
+        });
+    
+        $("#gem4").on("click", function(){
+            counter += crystalFour;
+            WinLoose();
+            $(playerScore).text(counter);
+            
+    
+        });
+    
+        function WinLoose (){
+    
+        if (counter === winNumber){
+            wins += 1;
+            alert("Winner!");
+            $(win).text(wins);
+            reset();
+    
+        } else if(counter > winNumber){
+            losses += 1;
+            alert("Bummer!");
+            $(loss).text(losses);
+            reset();
+        }
+        
+    }
+        
+    });
+    
+    
+    
+        
+    
+    
